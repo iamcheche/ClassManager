a0ed348271cf02c2b1451f2cacaa2713b9792285
iamcheche <chebalane@gmail.com>
2015-11-23 12:35:55 -0800

    Added Day and Date to Schedule Fragment

--- app/src/main/java/com/capstone/jhea/classmanager/ScheduleFragment.java
+++ app/src/main/java/com/capstone/jhea/classmanager/ScheduleFragment.java
@@ -6,16 +6,30 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Che Balane on 11/19/2015.
  */
 public class ScheduleFragment extends Fragment{
 
+    Calendar calendar;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View mainView = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        TextView date = (TextView) mainView.findViewById(R.id.date);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyy");
+        Date d = new Date();
+        String day = sdf.format(d);
+
+        date.setText(day);
 
         return mainView;
 
